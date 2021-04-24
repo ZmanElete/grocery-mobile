ed2ef5f23cffefded342d34e16ff500948bf619e
ZmanElete <zacharywright1@mail.weber.edu>
2021-04-24 13:38:12 -0600

    fixed comments

--- lib/services/api/user_api_service.dart
+++ lib/services/api/user_api_service.dart
@@ -1,5 +1,6 @@
 import 'package:grocery_list/models/user.dart';
 import 'package:grocery_list/services/api/rest_service.dart';
+import 'package:http/http.dart' as http;
 
 class UserApiService extends RestService<User> {
   static UserApiService get instance =>
@@ -16,7 +17,8 @@ class UserApiService extends RestService<User> {
 
   Future<User> current() async {
     //Retrieve always uses the request.user.id as the pk.
-    return get(0);
+    // /$resource/current/
+    return http.post();
   }
   //Only super users can user the delete function
   //All 'user' endpoints are filtered down to only the user that is accessing them.
